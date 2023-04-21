97098ed0a8fc8f9b79a694dbbc0f20f189519709
Gabriel-16n5 <gabrielgranjeia@alunos.utfpr.edu.br>
2023-04-21 02:51:44 -0300

    feito o requisito do entrada e saida de dados no back

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import { MongoClient } from "mongodb";
 import joi from "joi";
 import {v4 as uuid} from "uuid"
 import bcrypt from "bcrypt"
+import dayjs from "dayjs";
 const app = express();
 
 app.use(cors());
@@ -70,9 +71,37 @@ app.post("/", async (req, res) => {
 })
 
 app.post("/nova-transacao/:tipo", async (req, res) => {
-    
-    try{
+    const {valor, description} = req.body;
+    const {tipo} = req.params;
+    const {authorization} = req.headers;
+    if(tipo != "entrada" && tipo != "saida") return res.status(404).send("tipo errado");
+    const token = authorization?.replace("Bearer ", "")
 
+    const currencySchema = joi.object({
+        valor: joi.number().min(0).required(),
+        description: joi.string().required()
+    })
+    const validation = currencySchema.validate(req.body, {abortEarly: false});
+    if(validation.error) return res.status(422).send(validation.error.details);
+    try{
+        const validationToken = await db.collection("sessions").findOne({token});
+        if(!validationToken) return res.status(401).send("Invalid token");
+        if(tipo === "entrada"){
+            await db.collection("incoming").insertOne({
+                idUser: validationToken.idUser,
+                token: validationToken.token,
+                valor: valor,
+                data: dayjs().format('DD/MM')
+            })
+        } else if (tipo === "saida"){
+            await db.collection("out").insertOne({
+                idUser: validationToken.idUser,
+                token: validationToken.token,
+                valor: valor,
+                data: dayjs().format('DD/MM')
+            })
+        }
+        res.send("pamonha") 
     } catch (erro) {
         return res.status(500).send(erro.message)
     }
